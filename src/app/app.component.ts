12ef0374b10acd6170488e6b4d3d2504d97d7d82
DaveAldon <crawford@mail.gvsu.edu>
2019-09-05 11:46:53 -0400

    Added progress on lazy loading of images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styles: [ `
+    ngui-in-view {
+      min-height: 300px;
+    }
+    .num-images { 
+      position: fixed; padding: 5px;
+      bottom: 0; right: 0; background: #333;color: #fff;
+    }` ]
 })
 export class AppComponent  {
-  name = 'Angular';
+
+  get numImages(): number {
+    return this.element.nativeElement.querySelectorAll('img').length;
+  }
+
+  numArr = Array.from(Array(100), (_,x) => x);
+
+  constructor(public element: ElementRef) {}
+
 }
